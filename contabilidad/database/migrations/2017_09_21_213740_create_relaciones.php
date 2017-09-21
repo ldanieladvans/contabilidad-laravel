f444f44223bcde701c1c83c9784c5fde418990a5
ADVANS\mmabel <mmabel@advans.mx>
2017-09-21 17:29:59 -0500

    terminando migrations

--- contabilidad/database/migrations/2017_09_21_213740_create_relaciones.php
+++ contabilidad/database/migrations/2017_09_21_213740_create_relaciones.php
@@ -14,20 +14,14 @@ class CreateRelaciones extends Migration
     public function up()
     {
 
-        Schema::table('comp', function($table)
-            {
-                $table->integer('comp_asiento_id')->unsigned();
-                $table->foreign('comp_asiento_id')->references('id')->on('asiento')->onDelete('set null');
-
-            });
-
+    
         Schema::table('comprel', function($table)
             {
                 $table->integer('comprel_comp_id')->unsigned();
-                $table->foreign('comprel_comp_id')->references('id')->on('comp')->onDelete('set null');
+                $table->foreign('comprel_comp_id')->references('id')->on('comp')->onDelete('cascade');
 
                 $table->integer('comprel_comp_rel_id')->unsigned();
-                $table->foreign('comprel_comp_rel_id')->references('id')->on('comp')->onDelete('set null');
+                $table->foreign('comprel_comp_rel_id')->references('id')->on('comp')->onDelete('cascade');
 
             });
 
@@ -36,82 +30,82 @@ class CreateRelaciones extends Migration
         Schema::table('asiento', function($table)
             {
                 $table->integer('asiento_ctacont_id')->unsigned();
-                $table->foreign('asiento_ctacont_id')->references('id')->on('ctacont')->onDelete('set null');
+                $table->foreign('asiento_ctacont_id')->references('id')->on('ctacont');
 
                 $table->integer('asiento_polz_id')->unsigned();
-                $table->foreign('asiento_polz_id')->references('id')->on('polz')->onDelete('set null');
+                $table->foreign('asiento_polz_id')->references('id')->on('polz')->onDelete('cascade');
 
             });
 
         Schema::table('ctacont', function($table)
             {
-                $table->integer('ctacont_tiposubcta_id')->unsigned();
+                $table->integer('ctacont_tiposubcta_id')->unsigned()->nullable();
                 $table->foreign('ctacont_tiposubcta_id')->references('id')->on('tiposubcta')->onDelete('set null');
             });
 
         Schema::table('period', function($table)
             {
                 $table->integer('period_ejerc_id')->unsigned();
-                $table->foreign('period_ejerc_id')->references('id')->on('ejerc')->onDelete('set null');
+                $table->foreign('period_ejerc_id')->references('id')->on('ejerc')->onDelete('cascade');
             });
 
         Schema::table('polz', function($table)
             {
-                $table->integer('polz_period_id')->unsigned();
+                $table->integer('polz_period_id')->unsigned()->nullable();
                 $table->foreign('polz_period_id')->references('id')->on('period')->onDelete('set null');
             });
 
         Schema::table('blnza', function($table)
             {
-                $table->integer('blnza_period_id')->unsigned();
+                $table->integer('blnza_period_id')->unsigned()->nullable();
                 $table->foreign('blnza_period_id')->references('id')->on('period')->onDelete('set null');
 
-                $table->integer('blnza_ctacont_id')->unsigned();
+                $table->integer('blnza_ctacont_id')->unsigned()->nullable();
                 $table->foreign('blnza_ctacont_id')->references('id')->on('ctacont')->onDelete('set null');
             });
 
         Schema::table('provis', function($table)
             {
                 $table->integer('provis_comp_id')->unsigned();
-                $table->foreign('provis_comp_id')->references('id')->on('comp')->onDelete('set null');
+                $table->foreign('provis_comp_id')->references('id')->on('comp')->onDelete('cascade');
             });
 
         Schema::table('pago', function($table)
             {
                 $table->integer('pago_comp_id')->unsigned();
-                $table->foreign('pago_comp_id')->references('id')->on('comp')->onDelete('set null');
+                $table->foreign('pago_comp_id')->references('id')->on('comp')->onDelete('cascade');
 
-                $table->integer('pago_polz_id')->unsigned();
+                $table->integer('pago_polz_id')->unsigned()->nullable();
                 $table->foreign('pago_polz_id')->references('id')->on('polz')->onDelete('set null');
             });
 
         Schema::table('pagoimp', function($table)
             {
                 $table->integer('pagoimp_pago_id')->unsigned();
-                $table->foreign('pagoimp_pago_id')->references('id')->on('pago')->onDelete('set null');
+                $table->foreign('pagoimp_pago_id')->references('id')->on('pago')->onDelete('cascade');
             });
 
         Schema::table('impdet', function($table)
             {
                 $table->integer('impdet_pagoimp_id')->unsigned();
-                $table->foreign('impdet_pagoimp_id')->references('id')->on('pagoimp')->onDelete('set null');
+                $table->foreign('impdet_pagoimp_id')->references('id')->on('pagoimp')->onDelete('cascade');
             });
 
         Schema::table('polzcomp', function($table)
             {
                 $table->integer('polzcomp_polz_id')->unsigned();
-                $table->foreign('polzcomp_polz_id')->references('id')->on('polz')->onDelete('set null');
+                $table->foreign('polzcomp_polz_id')->references('id')->on('polz')->onDelete('cascade');
 
                 $table->integer('polzcomp_comp_id')->unsigned();
-                $table->foreign('polzcomp_comp_id')->references('id')->on('comp')->onDelete('set null');
+                $table->foreign('polzcomp_comp_id')->references('id')->on('comp')->onDelete('cascade');
             });
 
         Schema::table('pagorel', function($table)
             {
                 $table->integer('pagorel_pago_id')->unsigned();
-                $table->foreign('pagorel_pago_id')->references('id')->on('pago')->onDelete('set null');
+                $table->foreign('pagorel_pago_id')->references('id')->on('pago')->onDelete('cascade');
 
-                $table->integer('pagorel_asiento_id')->unsigned();
+                $table->integer('pagorel_asiento_id')->unsigned()->nullable();
                 $table->foreign('pagorel_asiento_id')->references('id')->on('asiento')->onDelete('set null');
             });
 
@@ -143,7 +137,7 @@ class CreateRelaciones extends Migration
                 $table->integer('tipcliente_cta_por_cobrar_id')->unsigned()->nullable();
                 $table->foreign('tipcliente_cta_por_cobrar_id')->references('id')->on('ctacont')->onDelete('set null');
 
-                $table->integer('tipcliente_cta_anticp_client_id')->unsigned();
+                $table->integer('tipcliente_cta_anticp_client_id')->unsigned()->nullable();
                 $table->foreign('tipcliente_cta_anticp_client_id')->references('id')->on('ctacont')->onDelete('set null');
 
             });
@@ -180,20 +174,20 @@ class CreateRelaciones extends Migration
                 $table->integer('cliente_direc_id')->unsigned()->nullable();
                 $table->foreign('cliente_direc_id')->references('id')->on('direc')->onDelete('set null');
 
-                $table->integer('cliente_cta_anticp_client_id')->unsigned();
+                $table->integer('cliente_cta_anticp_client_id')->unsigned()->nullable();
                 $table->foreign('cliente_cta_anticp_client_id')->references('id')->on('ctacont')->onDelete('set null');
             });
 
          Schema::table('prodingr', function($table)
             {
                 $table->integer('prodingr_tipcliente_id')->unsigned()->nullable();
-                $table->foreign('prodingr_tipcliente_id')->references('id')->on('tipcliente')->onDelete('set null');
+                $table->foreign('prodingr_tipcliente_id')->references('id')->on('tipcliente')->onDelete('cascade');
 
                 $table->integer('prodingr_cliente_id')->unsigned()->nullable();
-                $table->foreign('prodingr_cliente_id')->references('id')->on('cliente')->onDelete('set null');
+                $table->foreign('prodingr_cliente_id')->references('id')->on('cliente')->onDelete('cascade');
 
                 $table->integer('prodingr_cta_ingr_id')->unsigned();
-                $table->foreign('prodingr_cta_ingr_id')->references('id')->on('ctacont')->onDelete('set null');
+                $table->foreign('prodingr_cta_ingr_id')->references('id')->on('ctacont')->onDelete('cascade');
             });
 
          Schema::table('tipprov', function($table)
@@ -222,7 +216,7 @@ class CreateRelaciones extends Migration
                 $table->integer('tipprov_cta_por_pagar_id')->unsigned()->nullable();
                 $table->foreign('tipprov_cta_por_pagar_id')->references('id')->on('ctacont')->onDelete('set null');
 
-                $table->integer('tipprov_cta_anticp_prov_id')->unsigned();
+                $table->integer('tipprov_cta_anticp_prov_id')->unsigned()->nullable();
                 $table->foreign('tipprov_cta_anticp_prov_id')->references('id')->on('ctacont')->onDelete('set null');
             });
 
@@ -258,20 +252,20 @@ class CreateRelaciones extends Migration
                 $table->integer('proveed_direc_id')->unsigned()->nullable();
                 $table->foreign('proveed_direc_id')->references('id')->on('direc')->onDelete('set null');
 
-                $table->integer('proveed_cta_anticp_prov_id')->unsigned();
+                $table->integer('proveed_cta_anticp_prov_id')->unsigned()->nullable();
                 $table->foreign('proveed_cta_anticp_prov_id')->references('id')->on('ctacont')->onDelete('set null');
             });
 
          Schema::table('prodgast', function($table)
             {
                 $table->integer('prodgast_tipprov_id')->unsigned()->nullable();
-                $table->foreign('prodgast_tipprov_id')->references('id')->on('tipprov')->onDelete('set null');
+                $table->foreign('prodgast_tipprov_id')->references('id')->on('tipprov')->onDelete('cascade');
 
                 $table->integer('prodgast_proveed_id')->unsigned()->nullable();
-                $table->foreign('prodgast_proveed_id')->references('id')->on('proveed')->onDelete('set null');
+                $table->foreign('prodgast_proveed_id')->references('id')->on('proveed')->onDelete('cascade');
 
                 $table->integer('prodgast_cta_gast_id')->unsigned();
-                $table->foreign('prodgast_cta_gast_id')->references('id')->on('ctacont')->onDelete('set null');
+                $table->foreign('prodgast_cta_gast_id')->references('id')->on('ctacont')->onDelete('cascade');
             });
 
          Schema::table('bitac', function($table)
