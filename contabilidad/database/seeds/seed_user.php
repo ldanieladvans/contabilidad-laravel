ffbbf93a073b676bcc9a21494ff81cb6c11e4179
ADVANS\mmabel <mmabel@advans.mx>
2017-10-16 17:25:51 -0500

    quitando nick de seed de usuarios

--- contabilidad/database/seeds/seed_user.php
+++ contabilidad/database/seeds/seed_user.php
@@ -14,14 +14,12 @@ class seed_user extends Seeder
     {
         $apiUser = User::create([
             'name' => 'Usuario API',
-            'users_nick' => 'api',
             'email' => 'api@advans.mx', 
             'password' => bcrypt('Usuarioapi123*'),
         ]);
 
         $adminUser = User::create([
             'name' => 'Usuario Admin',
-            'users_nick' => 'admin',
             'email' => 'admin@advans.mx', 
             'password' => bcrypt('Admin123*'),
         ]);
